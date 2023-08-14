52f7da992907c96058bcf38e7e2c1cc738f6e78d
samani siewe darix <samanidarix@gmail.com>
2023-08-14 05:56:45 +0100

    models defined

--- models/posts.py
+++ models/posts.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import Relationship
 from database.database import Base
-
+from sqlalchemy.orm import relationship
 
 class Post(Base):
 
@@ -10,4 +10,8 @@ class Post(Base):
     id: int = Column(Integer, primary_key=True, index=True)
     title: str = Column(String,)
     content: str = Column (String)
-    user_id: int = Relationship("users.id")
+    user_id: int = Column(Integer, ForeignKey('users.id'))
+    post = relationship("Post", back_populates="users")
+
+    def __repr__(self):
+        return f'<Post "{self.content[:20]}...">'
--- models/users.py
+++ models/users.py
@@ -1,9 +1,14 @@
 from database.database import Base
 from typing import EmailStr
 from sqlalchemy import Column, String, Integer
-
+from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
     id: int = Column(Integer, primary_key=True, index=True)
     email : EmailStr = Column(String, index=True)
     fullname : str = Column(String,)
+
+    posts = relationship("Post", back_populates="customer")
+
+    def __repr__(self):
+        return f'<User "{self.email}...">'
