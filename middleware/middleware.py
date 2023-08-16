b4767fb7579637afb6c813dc24847c853b565207
samani siewe darix <samanidarix@gmail.com>
2023-08-16 22:29:48 +0100

    add params

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -17,10 +17,17 @@ class CollectMiddleware(BaseHTTPMiddleware):
         # i want to collect data from Request object to redisTimeseris with this midddleware
 
         client_host = request.client.host
-        rewuest_path = request.url.path
+        request_path = request.url.path
         request_methode = request.method
         schema_request = request.url.scheme
         content_type = request.headers.get('Content-Type')
+        user_agent = request.headers.get("user-agent")
+        host = request.headers.get("host")
+        connection = request.headers.get("connection")
+        content_length = request.headers.get("content-length")
+        referer = request.headers.get("referer")
+        accept_encoding = request.headers.get("accept-encoding")
+        origin = request.headers.ge("origin")
         header_request = request.headers
         request_port = request.url.port
         query_parametre_request = request.query_params
@@ -31,7 +38,7 @@ class CollectMiddleware(BaseHTTPMiddleware):
         time = datetime.now()
 
         content_type = request.headers.get('Content-Type')
-        print(f"content_type: {content_type}")
+        print(f"content_type: {content_type}, client_host: {client_host}, request_path : {request_path}, request_methode: {request_methode }, schema_request : {schema_request}, header_request : {header_request}, request_port : {request_port}, query_parametre_request : {query_parametre_request} path_parametre_request : {path_parametre_request}, cokies_request : {cokies_request}, user_agent: {user_agent}, host : {host}, connection : {connection}, referer : {referer}, content_length : {content_length} accept-encoding : {accept_encoding} origin: {origin}")
         
         # process the request and get the response    
         response = await call_next(request)
